aa8af7921d1de9af313eeafe45f99689edb51065
nsi-lfv-ata <65135283+nsi-lfv-ata@users.noreply.github.com>
2021-05-01 12:59:24 +0200

    Update adam_fonc.py

--- adam_fonc.py
+++ adam_fonc.py
@@ -5,19 +5,21 @@ im = Image.open("Elon_Musk.jpg")
 print(im.size)
 
 def adam():
-    for i in range(5):
-        for c in range(3):
-            for x in range(0+i*200,100+i*200):
-                for y in range(0+c*200,100+c*200):
+    l = int(input("la longeur du rectangle en pixel?(C'est mieux de choisir un diviseur de 900)"))
+    L = int(input("la largeur du rectangele?(C'est mieux de choisir un diviseur de 500)"))
+    for i in range(int((900/2+l/2)/l)):
+        for c in range(int((500/2+L/2)/L)):
+            for x in range(0+i*l*2,l+i*l*2):
+                for y in range(0+c*L*2,L+c*L*2):
                     pixel = im.getpixel((x,y))
                     p_rouge =255-int(pixel[0])
                     p_vert =255-int(pixel[1]) 
                     p_bleu =255-int(pixel[2])
                     im.putpixel((x,y),(p_rouge,p_vert,p_bleu))
-    for i in range(4):
-        for c in range(2):
-            for x in range(100+i*200,200+i*200):
-                for y in range(100+c*200,200+c*200):
+    for i in range(int((900/2)/l)):
+        for c in range(int((500/2)/L)):
+            for x in range(l+i*l*2, l*2+i*l*2):
+                for y in range(L+c*L*2,L*2+c*L*2):
                     pixel = im.getpixel((x,y))
                     p_rouge =255-int(pixel[0])
                     p_vert =255-int(pixel[1]) 
